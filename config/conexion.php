2ab27f9ad2ba843abff93bc49f7596f1af2a45b8
Marc Colom <marc18cb@gmail.com>
2024-04-16 23:34:57 +0200

    ya funciona

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+$uri = "mysql://avnadmin:AVNS_SV3vxOevkckSb-vKih-@extraescolar-msolizrueda-948a.b.aivencloud.com:18078/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try  {
+    $db = new PDO($conn, $fields['user'], $fields['pass']);
+    $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro de conexion: " . $e->getMessage();
+}
+?>
--- index.php
+++ index.php
@@ -1,25 +1,14 @@
+<?php require('./config/conexion.php') ?>
 <?php
+    try {
+        $statement = $db -> prepare('SELECT * FROM personal');
+        $statement->execute();
+        $results = $statement -> fetchAll(PDO::FETCH_ASSOC);
 
-$uri = "mysql://avnadmin:AVNS_SV3vxOevkckSb-vKih-@extraescolar-msolizrueda-948a.b.aivencloud.com:18078/defaultdb?ssl-mode=REQUIRED";
-
-$fields = parse_url($uri);
-
-// build the DSN including SSL settings
-$conn = "mysql:";
-$conn .= "host=" . $fields["host"];
-$conn .= ";port=" . $fields["port"];;
-$conn .= ";dbname=defaultdb";
-$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
-
-try {
-    $db = new PDO($conn, $fields["user"], $fields["pass"]);
-
-    $stmt = $db->query("SHOW TABLES");
-    $tables = $stmt->fetchAll(PDO::FETCH_NUM);
-    echo "Tables in the database:\n";
-    foreach ($tables as $table) {
-        echo $table[0] . "\n";
+        foreach ($results as $row) {
+            echo 'ID: ' . $row['DNI'] . '<br>';
+        }
+    } catch (PDOException $e) {
+        echo "Error de consulta: " . $e->getMessage();
     }
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
+?>
