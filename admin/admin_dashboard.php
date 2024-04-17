60726becf852c3b3394146fe7c6bfd2afeccdcb7
Mateo Daniel Soliz Rueda <mateo@cometalabs.io>
2024-04-17 18:50:06 +0200

    asd

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -2,11 +2,11 @@
     session_start();
 
     //Verificar si el usuario esta autenticado
-    if (!isset($_SESSION['username'])) {
-        header('Location: index.php');
+    if (!isset($_SESSION['email'])) {
+        header('Location: ../index.php');
         exit();
     }
 
-    echo "Bienvenido " . $_SESSION['username'];
+    echo "Bienvenido " . $_SESSION['email'];
 
 ?>
