3b527678824cd9f7065f4f03aacda7e35963e811
Marccb18 <146984794+Marccb18@users.noreply.github.com>
2024-04-24 18:36:17 +0200

    haz pull

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,29 +1,110 @@
 <?php
     session_start();
-
-    //Verificar si el usuario esta autenticado
+    require('../config/conexion.php');
+    
     if (!isset($_SESSION['email']) || $_SESSION['rol'] != 'ADM') {
         header('Location: ../index.php');
         exit();
     }
-    if(isset($_POST['logout'])) {
+    if (isset($_POST['logout'])) {
         require_once('../config/logout.php');
         logout();
     }
+
+        $db = new PDO($conn, $fields['user'], $fields['pass']);
+        $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $showMaterias = $db->query("SELECT * FROM materia");
+        $materias = $showMaterias->fetchAll(PDO::FETCH_ASSOC);
+        $db = null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="icon" href="../assets/img/logoSJO.png">
-    <link rel="stylesheet" href="../assets/css/admin_dashboard.css">
+    <title>Profesor Dashboard</title>
+    <link rel="stylesheet" href="../assets/css/profesor_dashboard.css">
 </head>
 <body>
-    <h1>Hola <?= $_SESSION['username'] ?></h1>
-    <form action="admin_dashboard.php" method="post">
-        <input type="submit" name="logout" value="Cerrar Sesión">
-    </form>
+    <div id="aside">
+        <div id="titlelogo">
+            <img src="../assets/img/logoSJO.svg" alt="Logo SJO">
+            <p>Sant Josep Obrer</p>
+        </div>
+        <ul id="side-menu">
+            <li class="active">
+                <a href="#">
+                    <img src="../assets/img/icon-home.svg" alt="Home icon">
+                    Inicio
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="../assets/img/Vector.svg" alt="Students icon">
+                    Alumnos
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="../assets/img/library.svg" alt="Library icon">
+                    Sesiones
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="../assets/img/layout-grid.svg" alt="Layout icon">
+                    Materias
+                </a>
+            </li>
+        </ul>
+
+        <form action="profesor_dashboard.php" method="post">
+            <input type="submit" value="logout" name="logout">
+        </form>
+        
+    </div>
+    <div id="main">
+        <div id="content">
+            <div id="top-content">
+                <ul>
+                    <li class="active">
+                        <a href="#">Clases</a>
+                    </li>
+                    <li >
+                        <a href="#">Alumnos</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="title">
+                <h3>Inicio</h3>
+                <p>Busca entre todas tus clases</p>
+            </div>
+            <div id="filter">
+                <div id="clases">
+                    <p>Clases</p>
+                    <div id="select-container">
+                        <select name="clases">
+                            <option value="">Todas</option>
+                            <?php
+                                foreach ($materias as $materia) { ?>
+                                    <option value="<?= $materia['Nombre']?>"><?= $materia['Nombre'] ?></option>
+                            <?php } ?>
+                        </select>
+                        <img src="../assets/img/arrow-select.svg" alt="Arrow Select">
+                    </div>
+                </div>
+                <div id="fecha">
+                    <p>Fecha</p>
+                    <div id="date-container">
+                        <img src="../assets/img/Calendar.svg" alt="Calendar">
+                        <input type="date">
+                    </div>
+                </div>
+            </div>
+            <div id="main-content">
+                
+            </div>
+        </div>
+    </div>
 </body>
 </html>
