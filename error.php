d805c1589d475e3e817a51f6a06e076ef6db424e
Marccb18 <146984794+Marccb18@users.noreply.github.com>
2024-06-04 15:40:38 +0200

    menu tipo instagram en todas las paginas

--- error.php
+++ error.php
@@ -18,6 +18,24 @@
     <p>Ocurrió un error durante la conexión a la base de datos:</p>
     <p><?php echo $error; ?></p>
     <a href="./index.php">Volver al inicio</a>
-</body>
+<div id="mobile-menu">
+        <a href="./admin_dashboard.php" class="active">
+            <img src="../assets/img/icon-home.svg" alt="home-icon">
+        </a>
+        <a href="./gestion_users.php">
+            <img src="../assets/img/Vector.svg" alt="gestion-users-icon">
+        </a>
+        <a href="./gestion_materias.php">
+            <img src="../assets/img/layout-grid.svg" alt="gestion-materias-icon">
+        </a>
+        <a href="./perfil.php">
+            <img src="../assets/img/person.svg" alt="person-icon">
+        </a>
+        <form action="admin_dashboard.php" method="post">
+            <button type="submit" name="logout">
+                <img src="../assets/img/logout.svg" alt="logout-icon">
+            </button>
+        </form>
+    </div>
 <script src="../assets/js/index.js"></script>
 </html>
